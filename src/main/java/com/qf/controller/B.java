930aae32d4c8476e18bfaef883f365f06e224f8f
Zhangmiemie <1069267387@qq.com>
2019-11-28 22:05:28 +0800

    小宝在Controller包 添加了 B、C 添加了两个方法

--- src/main/java/com/qf/controller/B.java
+++ src/main/java/com/qf/controller/B.java
@@ -2,6 +2,6 @@ package com.qf.controller;
 
 public class B {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("asdasdasdas");
     }
 }
--- src/main/java/com/qf/controller/C.java
+++ src/main/java/com/qf/controller/C.java
@@ -2,6 +2,6 @@ package com.qf.controller;
 
 public class C {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("asdasdasdas");
     }
 }
